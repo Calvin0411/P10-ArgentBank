f2f0217d8ede8df8311899ba3b0d94923fc226bb
Calvin0411 <ramjeeawoncalvin@gmail.com>
2024-10-14 21:35:03 +0200

    Ajout du footer et du header
    J'ai ajouté le header et le footer de ma page

--- Frontend/argent-bank-frontend/src/App.jsx
+++ Frontend/argent-bank-frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header'; 
 import Home from './pages/HomePage';
+import Footer from './components/footer';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
